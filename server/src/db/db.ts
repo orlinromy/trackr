9998bda36b96143dc0c7f17574bde2428e3c5367
orlinromy <orlinromy@gmail.com>
2023-05-14 14:37:04 +0800

    fix: store db name, user and password in env

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,11 +1,13 @@
 import { Pool } from "pg";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const pool = new Pool({
-  user: "db_user",
-  password: "example",
+  user: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
   host: "localhost",
   port: 5432,
-  database: "jobapplication",
+  database: process.env.POSTGRES_DB_NAME,
 });
 
 export default pool;
